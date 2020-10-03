619296fc63d3804fada5ff679d0580042b756eff
Tyler Vaughn <69045229+tylerlvaughn@users.noreply.github.com>
2020-10-02 21:24:02 -0700

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,1 @@
-# http://localhost:8888/edit/Desktop/python-api-challenge/WeatherPy/api_keys.py#OpenWeatherMap API Key
-api_key = "03c73f742744b44d6e0ed9b3232eb9f1"
 
-# Google API Key
-g_key = "AIzaSyBapD8sVxvH-3Xpc_bxcWG0Z4Mb6ueSjuY"
